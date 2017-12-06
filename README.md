7645c80ff23127e0fb32226a816ce9a266ba632f
xLsDg <xlsdg@qq.com>
2017-12-06 09:38:01 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# dva-antd-typescript-starter
+# dva-antd-typescript-starter [![Build Status](https://travis-ci.org/xlsdg/dva-antd-typescript-starter.svg?branch=master)](https://travis-ci.org/xlsdg/dva-antd-typescript-starter)
 
 > Get started with Dva.js and Ant Design.
--- src/routes/IndexPage.tsx
+++ src/routes/IndexPage.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {
   connect
 } from 'dva';
+import {
+  Button
+} from 'antd';
 
 import styles from './IndexPage.css';
 
@@ -14,6 +17,9 @@ function IndexPage() {
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
       </ul>
+      <Button type="primary">
+        This is a button
+      </Button>
     </div>
   );
 }
